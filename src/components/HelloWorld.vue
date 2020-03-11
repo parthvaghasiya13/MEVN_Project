ffd15ec6156d116c1af5a38e06bb4179952be69f
parthvaghasiya13 <35321448+parthvaghasiya13@users.noreply.github.com>
2020-03-11 03:38:26 -0500

    day2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,72 +1,69 @@
 <template>
-  <!-- <v-gallery :images="list" :caption="true"  >
-  </v-gallery> -->
+  <!-- <section class = "posts" >
+   <img v-for="post in posts" v-bind:key="post" :src="post.download_url" contain height="100px" width="150px"/>
+  <v-gallery v-for="post in posts" v-bind:key="post" :images="post.download_url"  >
+    </v-gallery>
+  </section> -->
+
+    <!-- <section class = "posts">
+      <v-gallery :images={{posts.download_url}}  >
+      </v-gallery>
+    </section> -->
+<!-- 
+  <div id="posts">
+      <v-gallery :images="download_url">
+      </v-gallery>
+  </div> -->
+
+    <div class="posts">
+        <div v-for="post in posts" v-bind:key="post" :src="post" class="image-container">
+                <img :src="post.download_url" width= "160px">
+                <figcaption>{{post.author}}</figcaption>
+        </div>
+    </div>
 
   <!-- <div id="app">
     {{ info }}
   </div> -->
 
-  <ul v-if="posts && posts.length">
+  <!-- <ul v-if="posts && posts.length">
     <li v-for="post of posts" v-bind:key="post">
       <p><strong>{{post.author}}</strong></p>
       <p>{{post.id}}</p>
-   
-      <!-- <v-gallery :images="url" :caption="true">
-      </v-gallery> -->
     </li>
-  </ul>
+  </ul> -->
 
 </template>
 
 
 <script>
-// var dataURL = 'https://picsum.photos/v2/list';
-
-// var App = new Vue({
-//   el: '#app',
-//   data: {
-//     posts: [] // initialize empty array
-//   },
-//   mounted() { // when the Vue app is booted up, this is run automatically.
-//     var self = this // create a closure to access component in the callback below
-//     $.getJSON(dataURL, function(data) {
-//       self.posts = data;
-//     });
-//   }
-// })
 
 import axios from 'axios';
-// import Vue from 'vue';
 
-// new Vue({
-//   el: '#app',
-//   data(){
-//     return {
-//       info: null
-//     }
-//   },
-//   mounted() {
-//     axios
-//       .get('https://picsum.photos/v2/list')
-//       .then(response => (this.info = response))
-//   }
-// })
+  // export default{
+  //   data() {
+  //     return {
+  //       posts: [],
+  //       errors: []
+  //     }
+  //   },
 
-// export default {
-//   data(){
-//     return {
-//       list: [
-//         {title:'Image1',url:'https://picsum.photos/id/1/5616/3744'}
-//         ]
-//     }
-//   }
-// }
+  //   created() {
+  //     axios.get('https://picsum.photos/v2/list')
+  //     .then(response => {
+  //       this.posts = response.data
+  //     })
+  //     .catch(e => {
+  //       this.errors.push(e)
+  //     })
+  //   }
+  // }
 
   export default{
     data() {
       return {
         posts: [],
-        errors: []
+        error: []
       }
     },
 
@@ -83,3 +80,11 @@ import axios from 'axios';
 
 </script>
 
+<style scoped>
+.image-container {
+  display: table-cell;
+  vertical-align: middle;
+  height: 120px;
+  background-color: black;
+}
+</style>
