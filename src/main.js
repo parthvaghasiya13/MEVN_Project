2608d8b18016ecf3c71b4314e760f759010c2b3b
parthvaghasiya13 <35321448+parthvaghasiya13@users.noreply.github.com>
2020-03-11 01:53:43 -0500

    JSON parsing

--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap'
 import '@/assets/css/style.css'
+import vGallery from 'v-gallery'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 
 //require('@/assets/style.css')
 
+Vue.use(VueAxios, axios)
+Vue.use(vGallery)
 Vue.config.productionTip = false
 
 new Vue({
