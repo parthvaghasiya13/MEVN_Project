8843c364065197c05ecc31ef5a48b40e901c883c
parthvaghasiya13 <35321448+parthvaghasiya13@users.noreply.github.com>
2020-03-11 22:01:29 -0500

    different galleries

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <footer id="custom-footer">
-        <span class="mr-4">&copy; 2020 Fii USA</span>
+    <footer id="custom-footer" color=white>
+        <span class="mr-4">&copy; 2020 Foxconn Industrial Internet USA</span>
         <span>Terms of Service</span>
     </footer>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,61 @@
 <template>
-  <!-- <section class = "posts" >
-   <img v-for="post in posts" v-bind:key="post" :src="post.download_url" contain height="100px" width="150px"/>
-  <v-gallery v-for="post in posts" v-bind:key="post" :images="post.download_url"  >
-    </v-gallery>
-  </section> -->
-
-    <!-- <section class = "posts">
-      <v-gallery :images={{posts.download_url}}  >
-      </v-gallery>
-    </section> -->
-<!-- 
-  <div id="posts">
-      <v-gallery :images="download_url">
-      </v-gallery>
-  </div> -->
 
     <div class="posts">
-        <div v-for="post in posts" v-bind:key="post" :src="post" class="image-container">
-                <img :src="post.download_url" width= "160px">
-                <figcaption>{{post.author}}</figcaption>
-        </div>
+             <!-- <div v-for="post in posts" v-bind:key="post" :src="post" class="image-container" >
+                    <img :src="post.download_url" alt="post.url" width= "160px" margin="auto">
+                     <figcaption>{{post.author}}</figcaption>
+              </div> -->
+              <!-- <table id="table1">
+                <tr class="posts">
+                  <tr v-for="post in posts" v-bind:key="post" class="posts">
+                    <img :src="post.download_url" :alt="post.author" width= 50%>
+                    <figcaption class="center">{{post.author}}</figcaption>
+                    <br/>
+                  </tr>
+                </tr>
+                <tr class="posts">
+                  <td v-for="post in posts" v-bind:key="post">
+                    <img :src="post.download_url" :alt="post.author" width= 160px>
+                    <figcaption>{{post.author}}</figcaption>
+                  <td/>
+                  </tr> 
+              </table> -->
+
+             <!-- <div v-for="post in posts" v-bind:key="post" :src="post" class="image-container" >
+                    <img v-bind:src="post.download_url" alt="post.url" width= "160px" margin="auto">
+                     <figcaption>{{post.author}}</figcaption>
+              </div> -->
+
+              <div id="posts">
+                <div v-for="post in posts" v-bind:key="post">
+                  <img v-bind:src="post.download_url" alt="post.url" width= 200px>
+                  <figcaption>{{post.author}}</figcaption>
+                </div>
+              </div>
+                <!-- <div class="slider">
+                  <div v-for="post in posts" v-bind:key="post" :src="post" class="image-container">
+                    <img :src="post.download_url" alt="post.url" width= "160px" margin="auto">
+                     <figcaption>{{post.author}}</figcaption>
+                  </div>
+                </div> -->
+
+              <!-- <ul>
+                <ul class="posts">
+                  <li v-for="post in posts" v-bind:key="post">
+                    <img :src="post.download_url" :alt="post.author" width= 40%>
+                    <figcaption>{{post.author}}</figcaption>
+                  <li/>
+                    <li v-for="(post, index) in posts" :key="post"
+                       :class="'posts-' + ((index % 30) + 1)"
+                      >
+
+                     {{ post.author }}
+
+                    </li>
+                </ul>
+              </ul> -->
+          
     </div>
-
-  <!-- <div id="app">
-    {{ info }}
-  </div> -->
-
-  <!-- <ul v-if="posts && posts.length">
-    <li v-for="post of posts" v-bind:key="post">
-      <p><strong>{{post.author}}</strong></p>
-      <p>{{post.id}}</p>
-    </li>
-  </ul> -->
 
 </template>
 
@@ -39,25 +63,6 @@
 <script>
 
 import axios from 'axios';
-
-  // export default{
-  //   data() {
-  //     return {
-  //       posts: [],
-  //       errors: []
-  //     }
-  //   },
-
-  //   created() {
-  //     axios.get('https://picsum.photos/v2/list')
-  //     .then(response => {
-  //       this.posts = response.data
-  //     })
-  //     .catch(e => {
-  //       this.errors.push(e)
-  //     })
-  //   }
-  // }
 
   export default{
     data() {
@@ -77,7 +82,6 @@ import axios from 'axios';
       })
     }
   }
-
 </script>
 
 <style scoped>
@@ -85,6 +89,11 @@ import axios from 'axios';
   display: table-cell;
   vertical-align: middle;
   height: 120px;
-  background-color: black;
+  background-color: white;
+}
+
+#posts div {
+  display: inline-block;
+  margin: 6px 8px;
 }
 </style>
